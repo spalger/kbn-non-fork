bafe6845266906f78c36d1e125926477b2fc4e71
spalger <email@spalger.com>
2016-07-22 14:28:49 -0700

    teach git who we are

--- ci/scripts/setup_repo.sh
+++ ci/scripts/setup_repo.sh
@@ -9,5 +9,7 @@ fi
 
 # redirect tree output to /dev/null, warns/errs go to stderr
 git init
+git config --global user.email "kibana@ci.container"
+git config --global user.name "Kibana CI"
 git commit --allow-empty -m 'commit so we have a usable repo'
 npm install --quiet > /dev/null
