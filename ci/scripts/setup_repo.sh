0f41189a9db8f65a8c7132fe75a3fd3b801b55ec
spalger <email@spalger.com>
2016-07-22 13:48:44 -0700

    add a git repo so that tasks can rely on it being there

--- ci/scripts/setup_repo.sh
+++ ci/scripts/setup_repo.sh
@@ -8,4 +8,6 @@ if [ "$NODE_VERSION" != "$(cat .node-version)" ]; then
 fi
 
 # redirect tree output to /dev/null, warns/errs go to stderr
+git init
+git commit --allow-empty -m 'commit so we have a usable repo'
 npm install --quiet > /dev/null
