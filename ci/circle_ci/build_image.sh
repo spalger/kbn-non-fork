2289c45df0acd823a4f0429841bbb87ad7419a78
spalger <email@spalger.com>
2016-07-23 11:02:46 -0700

    -- document script with echo statements

--- ci/circle_ci/build_image.sh
+++ ci/circle_ci/build_image.sh
@@ -1,42 +1,60 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
-# we date our cache so that it is always rebuilt from scratch once a week
-date="$(date +%Y_%W)"
 cache_dir=~/docker-cache
 
+# the base image is super stable and docker's cache
+# should be fine as long as it's valid
 branch_sum="$(echo "$CIRCLE_BRANCH" | shasum | cut -d ' ' -f 1)"
+base_cache_name="base_image_branch_${branch_sum}.tar"
+base_tag="kibana-ci/base:branch_$branch_sum"
+echo " + base - cache: $base_cache_name tag: $base_tag"
 
-base_cache_name="base_image.tar"
-base_cache_path="$cache_dir/$base_cache_name"
-base_tag="kibana-ci/base:branch:$branch_sum"
-
-runner_cache_prefix="runner_image_"
-runner_cache_name="${runner_cache_prefix}${date}.tar"
-runner_cache_path="$cache_dir/$runner_cache_name"
+# unlike the base image, the runner image includes
+# npm dependencies that might behave unexpectedly, so
+# we include the year+week in the cache path to force a
+# full rebuild once a week
+date="$(date +%Y_%W)"
+runner_cache_prefix="runner_image_${branch_sum}"
+runner_cache_name="${runner_cache_prefix}_${date}.tar"
 runner_tag="kibana-ci/task-runner:$CIRCLE_SHA1"
+echo " + task_runner - cache: $runner_cache_name tag: $runner_tag"
 
 # ensure the cache directory exists
 mkdir -p $cache_dir
 
-# load of remove cache items
-caches="$(find $cache_dir -maxdepth 1 -name '*.tar')"
-for c in $caches; do
-  if [ "$c" == "$base_cache_path" ] || [ "$c" == "$runner_cache_path" ] ; then
-    docker load < "$c";
-  else
-    echo "removing outdated cache item $c";
-    rm -rfv "$c";
+# load or remove cache items
+echo " + checking image caches"
+cache_paths="$(find $cache_dir -maxdepth 1 -name '*.tar')"
+for path in $cache_paths; do
+  echo " ++ cache: $path"
+  name="$(basename $path)"
+
+  if [[ "$name" == "$runner_cache_name" ]] || [[ "$name" != "$base_cache_name" ]]; then
+    echo "  + loading cache into docker";
+    docker load < "$path"
+    continue;
   fi
+
+  if [[ "$name" == $runner_cache_prefix* ]]; then
+    echo "  + removing outdated runner cache";
+    rm -rf "$path";
+    continue;
+  fi
+
+  echo "  + ignoring irrelevant cache";
 done
 
-# build the base image
+echo " ++ base image"
+echo "  + building and tagging with '$base_tag'"
 docker build -t "$base_tag" --file ci/base/Dockerfile .
-# save the base image and it's upstream to cache
-docker save "$base_tag" buildpack-deps:xenial > "$base_cache_path"
+echo "  + caching image at '$cache_dir/$base_cache_name'"
+docker save "$base_tag" buildpack-deps:xenial > "$cache_dir/$base_cache_name"
 
-# build the runner image
+echo " ++ runner image"
+echo "  + building and tagging with '$runner_tag'"
 docker build -t "$runner_tag" --file ci/task_runner/Dockerfile .
-# save the runner image to cache
-docker save "$runner_tag" > "$runner_cache_path"
+
+echo "  + caching image at '$cache_dir/$runner_cache_name'"
+docker save "$runner_tag" > "$cache_dir/$runner_cache_name"
